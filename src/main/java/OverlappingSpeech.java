a9e9f310b8f38fc8b7add35b08d473f9cf6edd28
BuildTools <unconfigured@null.spigotmc.org>
2021-06-23 12:23:12 -0400

    Committing because my cat just turned off my computer and I don't want all my progress lost.

--- src/main/java/OverlappingSpeech.java
+++ src/main/java/OverlappingSpeech.java
@@ -2,20 +2,30 @@ import ai.djl.*;
 import ai.djl.inference.*;
 import ai.djl.ndarray.*;
 import ai.djl.translate.*;
-import ai.djl.pytorch.engine.*;
+
+import javax.sound.sampled.*;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.Arrays;
+
 
 public class OverlappingSpeech
 {
 
+    // Stores the model predictors
     private final Predictor<Double, Double> ovl_predictor;
     private final Predictor<Double, Double> enc_predictor;
     private final Predictor<Double, Double> mask_predictor;
     private final Predictor<Double, Double> dec_predictor;
 
+    // Store model parameters
+    private final int separator_fs = 8000;
+    private final int ovl_det_fs = 16000;
+    private final double window_length = 1; // Unit is seconds
+
     public OverlappingSpeech(String model_dir) throws MalformedModelException, IOException {
+
         // Define locations of the saved model paths
         Path model_path = Paths.get(model_dir);
         Model ovl_model = Model.newInstance("overlap_detection.zip");
@@ -64,13 +74,102 @@ public class OverlappingSpeech
 
     }
 
-    public double detectOverlappingSpeech(double[] audio, double sample_rate)
+    // TODO Implement this function
+    public AudioInputStream detectOverlappingSpeech(AudioInputStream audio_input)
     {
-        return 0.0;
+        AudioFormat input_format = audio_input.getFormat();
+
+        // Return this to prevent error for now
+        return audio_input;
     }
 
-    public double separateOverlappingSpeech(double[] audio, double sample_rate)
+    // TODO Finish this function
+    public AudioInputStream[] separateOverlappingSpeech(AudioInputStream audio_input) throws IOException
     {
-        return 0;
+        // Define array that will contain the separated audio streams
+        AudioInputStream separated_audio[] = new AudioInputStream[2];
+
+        // Grab the format of the audio
+        AudioFormat input_format = audio_input.getFormat();
+
+        // Determine if audio needs to be resampled
+        if (input_format.getSampleRate() != this.separator_fs)
+        {
+            AudioInputStream formatted_audio = resampleAudio(audio_input);
+        }
+        else
+        {
+            AudioInputStream formatted_audio = audio_input;
+        }
+
+        // Window the audio into the proper lengths to be fed through the system
+
+        // Convert the AudioInputStream arrays to float arrays for the models
+
+        // Run each window through the speech separation models
+
+        // Repackage the audio into two AudioInputStreams
+
+        return separated_audio;
     }
+
+    private AudioInputStream resampleAudio(AudioInputStream audio_input) throws IOException
+    {
+        // Make the audio format easier to access
+        AudioFormat input_format = audio_input.getFormat();
+
+        // Convert the audio frames to an array of floats for manipulation
+        double frames[] = convertToDoubles(audio_input);
+
+
+        // Return this to prevent error for now
+        return audio_input;
+    }
+
+    private AudioInputStream[] windowAudio(AudioInputStream audio_input)
+    {
+        // Put here to prevent errors when building. Will need to calculate amount of windows needed first.
+        AudioInputStream windows[] = new AudioInputStream[2];
+        return windows;
+    }
+
+    private double[] convertToDoubles(AudioInputStream audio_input) throws IOException
+    {
+        // Grab the format of the input audio
+        AudioFormat input_format = audio_input.getFormat();
+
+        // Define double array that will be returned
+        int audio_frames[] = new int[(int)audio_input.getFrameLength()];
+
+        // Create byte array from the audio input
+        byte [] audio_bytes = new byte[audio_input.available()];
+
+        // Loop through input and store the bytes
+        for (int i = 0; i < audio_bytes.length; i++)
+        {
+            audio_input.read(audio_bytes);
+        }
+
+        // Convert the bytes to doubles
+        for (int i = 0; i < audio_frames.length; i++)
+        {
+            int temp_int = 0;
+            for (int j = 0; j < input_format.getFrameSize(); j++)
+            {
+                temp_int = ((byte)temp_int | (audio_bytes[(i * input_format.getFrameSize()) + j] << (8 * j)));
+            }
+            audio_frames[i] = temp_int;
+        }
+
+        // Convert the ints to floats
+        double audio_doubles[] = Arrays.stream(audio_frames).asDoubleStream().toArray();
+
+        return audio_doubles;
+    }
+
+//    private  AudioInputStream convertToInputStream(double[] audio_input)
+//    {
+//
+//    }
+
 }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,11 +1,23 @@
 import ai.djl.*;
 
+import javax.sound.sampled.*;
+
+import java.io.File;
 import java.io.IOException;
 
 public class Test {
 
-    public static void main (String args[]) throws MalformedModelException, IOException
-    {
+    public static void main (String args[]) throws MalformedModelException, IOException, UnsupportedAudioFileException {
+        String file_path = ".\\src\\main\\resources\\mix.wav";
+        File audio_file = new File(file_path);
+
+        // Load the models
         OverlappingSpeech model = new OverlappingSpeech(".\\src\\main\\resources");
+
+        // Load the audio data
+        AudioInputStream audio = AudioSystem.getAudioInputStream(audio_file);
+
+        // Separate the mixed speech
+        model.separateOverlappingSpeech(audio);
     }
 }
